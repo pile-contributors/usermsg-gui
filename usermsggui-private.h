be44d2906e528a8434305785b686ac730c486347
Nicu Tofan <nicu.tofan@gmail.com>
2016-01-04 14:54:10 +0200

    Local changes

--- usermsggui-private.h
+++ usermsggui-private.h
@@ -1,7 +1,7 @@
 /**
  * @file usermsggui-private.h
  * @brief Declarations for UserMsgGui class
- * @author 
+ * @author Nicu Tofan <nicu.tofan@gmail.com>
  * @copyright Copyright 2014 piles contributors. All rights reserved.
  * This file is released under the
  * [MIT License](http://opensource.org/licenses/mit-license.html)
--- usermsglogw.h
+++ usermsglogw.h
@@ -1,7 +1,7 @@
 /**
  * @file usermsglogw.h
  * @brief Declarations for UserMsgLogW class
- * @author 
+ * @author Nicu Tofan <nicu.tofan@gmail.com>
  * @copyright Copyright 2014 piles contributors. All rights reserved.
  * This file is released under the
  * [MIT License](http://opensource.org/licenses/mit-license.html)
